f9927286b01b29dd03c114a9e42e9a0f5524d648
Akash Srivastava <ankitsri66610@gmail.com>
2022-05-23 10:31:27 +0530

    Some Changes

--- basics/variable_len_arguments.js
+++ basics/variable_len_arguments.js
@@ -3,5 +3,6 @@ function sum(a,b) {
     return a + b;
 }
 
-var result = sum(4);
+var result = sum(4), result2 = sum(4, 5, 6); // ignores the third argument
 console.log(result);
+console.log(result2);
