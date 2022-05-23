55bb92ac92b4d4d9879d5ee066273f632f281fcc
Akash Srivastava <ankitsri66610@gmail.com>
2022-05-23 14:35:33 +0530

    array object

---
+++ basics/arr.js
@@ -0,0 +1,14 @@
+var a = [1,2,3,4];
+var b = new Array(2,3,4,5);
+var c = new Array();
+var d = new Array(3);
+var e = new Array(2,3);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+var len = c.length, len2 = d.length, len3 = e.length;
+console.log(c, " ", d, " ", e);
