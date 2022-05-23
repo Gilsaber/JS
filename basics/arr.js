7c306890d0fc6ff0e7337ca31082e9a8252c9b0b
Akash Srivastava <ankitsri66610@gmail.com>
2022-05-23 14:50:22 +0530

    Splice fn.

--- basics/arr.js
+++ basics/arr.js
@@ -10,5 +10,21 @@ console.log(c);
 console.log(d);
 console.log(e);
 
+c.push(5); // insertion at the end
+c.push(6);
+c.pop(); // removes last element and return it as well
+
+e.push(7); e.push(8); e.pop();
+
 var len = c.length, len2 = d.length, len3 = e.length;
 console.log(c, " ", d, " ", e);
+
+var arr = [1,2,3,4,5];
+arr.splice(1,1); // splice(starting index, deletion count, elements to be inserted)
+console.log(arr);
+
+arr.splice(1,0,15); // Insertion only 
+console.log(arr);
+
+arr.splice(2,2,40);
+console.log(arr);
