2d68417c762f7de665e486beacaf072ca59a6844
Akash Srivastava <ankitsri66610@gmail.com>
2022-05-24 12:16:41 +0530

    other methods

--- basics/set_Timeout.js
+++ basics/set_Timeout.js
@@ -1,5 +1,12 @@
+var sec = 1;
 function hello() {
-    console.log("hello world");
+    console.log("after", sec);
+    sec++;
+
+    if(sec === 6) {
+        clearInterval(id);
+    }
 }
 
-setTimeout(hello, 2000);
+// setTimeout(hello, 2000);
+var id = setInterval(hello, 2000);
